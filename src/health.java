8bb6132bba715649f9d8e0a0a20f728fae6aa403
CMacSwam <c-mccluskey.1@onu.edu>
2025-03-18 15:02:56 -0400

    Commit2

--- src/health.java
+++ src/health.java
@@ -1,2 +1,26 @@
 public class health {
+    int maxHealth = 100;
+    int currentHealth;
+
+    public void heal(int addHealth){
+        currentHealth += addHealth;
+        if(currentHealth > maxHealth) currentHealth = maxHealth;
+    }
+
+    public void takeDamage(int damage){
+        currentHealth -= damage;
+        if(currentHealth <= 0) currentHealth = 0;
+    }
+
+    public boolean isAlive(){
+        return currentHealth > 0;
+    }
+
+    public int getCurrentHealth(){
+        return currentHealth;
+    }
+
+    public int getMaxHeath(){
+        return maxHealth;
+    }
 }
--- src/player.java
+++ src/player.java
@@ -1,2 +1,27 @@
 public class player {
+    double health;
+    double lastDamage = 0;
+    double currentDamage = 0;
+    private boolean Gender; //true if male, false if female
+
+    public player(){
+        health = 100;
+        this.setGender(Gender);
+    }
+
+    public void setGender(boolean select){
+        Gender = select;
+    }
+    public double getDamage(double damageDone){
+        lastDamage = currentDamage;
+        return currentDamage = damageDone;
+    }
+
+    public double getHealth(){
+        return health;
+    }
+
+    public double getLastDamage() {
+        return lastDamage;
+    }
 }
